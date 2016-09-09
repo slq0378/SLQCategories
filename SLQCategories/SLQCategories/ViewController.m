c0ff76bdc23b2d2a01e85077c77b6e7b768d59bc
slq0378 <slq0378@gmial.com>
2016-09-09 17:55:35 +0800

    Revert "add resource"
    This reverts commit 42813f13239c4bf95f6cb2ec10a3cf9e4aa1dcef.

--- SLQCategories/SLQCategories/ViewController.m
+++ SLQCategories/SLQCategories/ViewController.m
@@ -7,11 +7,9 @@
 //
 
 #import "ViewController.h"
-#import "UIImage+Cateogries.h"
 
 @interface ViewController ()
-/**图片*/
-@property (nonatomic, strong) UIImageView *imageView;
+
 @end
 
 @implementation ViewController
@@ -19,19 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 200)];
-    [self.view addSubview:self.imageView];
-    self.imageView.image = [UIImage imageNamed:@"1"];
-    
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    self.imageView.image = [UIImage gaussianBlurImage:[UIImage imageNamed:@"1"] andInputRadius:5];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 @end
