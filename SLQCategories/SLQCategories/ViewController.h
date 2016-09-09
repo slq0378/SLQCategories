cce3dabe50dbdd779b5bf7a1aa9b57229eb04f5e
slq0378 <slq0378@gmial.com>
2016-09-09 17:35:17 +0800

    init project

---
+++ SLQCategories/SLQCategories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLQCategories
+//
+//  Created by Christian on 16/9/9.
+//  Copyright © 2016年 MrSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SLQCategories/SLQCategories/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SLQCategories
+//
+//  Created by Christian on 16/9/9.
+//  Copyright © 2016年 MrSong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
