0eff70f7d134491f3088d4aa919c0d7e583a7ed4
slq0378 <slq0378@gmial.com>
2016-09-23 13:11:33 +0800

    change header

--- SLQCategories/Categories/AllCategoriesHeader.h
+++ SLQCategories/Categories/AllCategoriesHeader.h
@@ -10,7 +10,14 @@
 #define AllCategoriesHeader_h
 #import "UIImageCateogriesHeader.h"
 #import "UIButtonCategoriesHeader.h"
+#import "UIControl+Block.h"
+#import "UIDevice+Hardware.h"
+#import "UIColorCategoriesHeader.h"
+#import "UIBezierPath+BasicShapes.h"
+#import "UIBezierPath+SVG.h"
 
-
-
+#import "UIBezierPath+Symbol.h"
+#import "UIAlertView+Block.h"
+#import "UIApplication+ApplicationSize.h"
+#import "UIApplication+Permissions.h"
 #endif /* AllCategoriesHeader_h */
